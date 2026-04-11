4cce4f62cf063f7533e2a95d715cfa752c0c8834
IRF Company <irfadministracion@gmail.com>
2026-04-11 12:14:26 -0600

    Fix robots sitemap URL to use www domain

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://irf.com.mx/sitemap.xml",
+    sitemap: "https://www.irf.com.mx/sitemap.xml",
   };
 }
